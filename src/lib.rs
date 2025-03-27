0d172ec5b67f685f5238638a9dfaadf2e5c9dacb
XdotCore <xandercorvalan@gmail.com>
2025-03-26 22:46:59 -0500

    Initial Commit

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+use dlopen::wrapper::{Container, WrapperApi};
+use dlopen_derive::WrapperApi;
+
+#[derive(WrapperApi)]
+struct BBApi {
+    start_bluebrick: extern "C" fn(platform: Platform, renderer: Renderer)
+}
+
+#[repr(C)]
+#[derive(PartialEq, Clone, Copy)]
+pub enum Renderer {
+    DX9
+}
+
+#[repr(C)]
+#[derive(PartialEq, Clone, Copy)]
+pub enum Platform {
+    Win32
+}
+
+pub fn load_bluebrick(platform: Platform, renderer: Renderer) {
+    let bluebrick = match unsafe { Container::<BBApi>::load("bluebrick/bluebrick") } {
+        Ok(bb) => bb,
+        Err(e) => {
+            let _ = msgbox::create("Error Loading BlueBrick", &format!("Problem opening loader:\n{e:?}"), msgbox::IconType::Error);
+            return;
+        }
+    };
+
+    bluebrick.start_bluebrick(platform, renderer);
+
+    std::mem::forget(bluebrick); // keeps from dropping, which would unload bluebrick
+}
